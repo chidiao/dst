aacea69f0f8f56800b52768ba9091d3bed15da3b
孙颖洲 <sunyz94123@qq.com>
2024-08-09 21:43:44 +0800

    feat: mods

---
+++ mods/多层世界.lua
@@ -0,0 +1,28 @@
+return {
+  ["workshop-1754389029"]={
+    configuration_options={
+      always_show_ui=true,
+      auto_balancing=false,
+      default_galleryful=0,
+      force_population=false,
+      gift_toasts_offset=100,
+      ignore_sinkholes=false,
+      migration_postern=false,
+      migrator_required=false,
+      name_button=true,
+      no_bat=true,
+      open_button=true,
+      say_dest=true,
+      world_config={
+        ["1"]={ category="森林", name="花果山", note="加群[323741946]一起玩！", galleryful=12, is_cave=false },
+        ["2"]={ category="洞穴", name="水帘洞", note="加群[323741946]一起玩！", galleryful=12, is_cave=true },
+        ["3"]={ category="森林", name="盘丝岭", note="加群[323741946]一起玩！", galleryful=12, is_cave=false },
+        ["4"]={ category="洞穴", name="盘丝洞", note="加群[323741946]一起玩！", galleryful=12, is_cave=true },
+        ["5"]={ category="森林", name="骷髅山", note="加群[323741946]一起玩！", galleryful=12, is_cave=false },
+        ["6"]={ category="洞穴", name="白骨洞", note="加群[323741946]一起玩！", galleryful=12, is_cave=true } 
+      },
+      world_prompt=false 
+    },
+    enabled=true 
+  } 
+}
